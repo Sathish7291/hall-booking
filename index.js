83fe4f0f1f801403b715001f500c60674fd523e2
sathish7291 <sathish010395@gmail.com>
2024-08-30 21:57:52 +0530

    hall-booking

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import AppRoutes from '../Hall booking/src/routes/index.js';
+import AppRoutes from './src/routes/index.js';
 
 const app = express()
 const PORT = process.env.PORT  || 8000;
 app.use(express.json())
 
-app.use('/',AppRoutes)
+app.use(AppRoutes)
 
 app.listen(PORT, ()=> console.log(`App is listening at ${PORT}`))
